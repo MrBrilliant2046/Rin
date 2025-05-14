dd0790510fe659d2df33412f6d901d5aea2b2435
Brilliant <94728421+MrBrilliant2046@users.noreply.github.com>
2025-05-14 14:12:07 +0800

    Update migrator.ts

--- scripts/migrator.ts
+++ scripts/migrator.ts
@@ -49,7 +49,7 @@ node_compat = true
 crons = ["*/20 * * * *"]
 
 [vars]
-FRONTEND_URL = "${FRONTEND_URL}"
+FRONTEND_URL = "https://blog.lolicons.top"
 S3_FOLDER = "${S3_FOLDER}"
 S3_CACHE_FOLDER="${S3_CACHE_FOLDER}"
 S3_REGION = "${S3_REGION}"
