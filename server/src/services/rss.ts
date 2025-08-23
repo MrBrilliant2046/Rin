36cb6fca38055764b221f491f88f898b600f9f26
Brill <94728421+MrBrilliant2046@users.noreply.github.com>
2025-08-23 14:57:37 +0800

    Update rss.ts
    Update name

--- server/src/services/rss.ts
+++ server/src/services/rss.ts
@@ -65,7 +65,7 @@ export async function rssCrontab(env: Env) {
     const faviconKey = getFaviconKey();
 
     let feedConfig: any = {
-        title: "Lolicon",
+        title: "ロリコン",
         description: env.RSS_DESCRIPTION || "Feed from Rin",
         id: frontendUrl,
         link: frontendUrl,
