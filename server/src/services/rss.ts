0119427ff52676716dc87ddad7761f8a5ab9eef6
Brill <94728421+MrBrilliant2046@users.noreply.github.com>
2025-08-23 15:03:14 +0800

    Update rss.ts
    Update title

--- server/src/services/rss.ts
+++ server/src/services/rss.ts
@@ -60,7 +60,7 @@ export function RSSService() {
 export async function rssCrontab(env: Env) {
     const frontendUrl = `${(env.FRONTEND_URL.startsWith("http://") || env.FRONTEND_URL.startsWith("https://") ? '' :'https://')}${env.FRONTEND_URL}`;
     const db = drizzle(env.DB, { schema: schema })
-    let title = "Lolicon";
+    let title = "ロリコン";
     const description = env.RSS_DESCRIPTION || "Feed from Rin";
     if (!title) {
         const user = await db.query.users.findFirst({ where: eq(users.id, 1) });
@@ -75,7 +75,7 @@ export async function rssCrontab(env: Env) {
         id: frontendUrl,
         link: frontendUrl,
         favicon: `${frontendUrl}/favicon.png`,
-        copyright: "All rights reserved 2024",
+        copyright: "All rights reserved 2025",
         updated: new Date(), // optional, default = today
         generator: "Feed from Rin", // optional, default = 'Feed for Node.js'
         feedLinks: {
